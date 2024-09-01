52b63aaf128ff8b129bdbbadc60f5074e1ff53a9
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-02 02:33:53 +0800

    Time: 22 ms (83.08%) | Memory: 6.5 MB (95.94%) - LeetSync

--- 792-binary-search/binary-search.go
+++ 792-binary-search/binary-search.go
@@ -1,7 +1,7 @@
 func search(nums []int, target int) int {
     l,r := 0, len(nums)-1
     for l <= r {
-        m := (l+r)/2
+        m := l + (r-l)/2
         if nums[m] == target { return m }
         if nums[m] > target { r = m-1 }
         if nums[m] < target { l = m+1 }

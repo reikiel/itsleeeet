4b1a960c7e429dac6e6abc64f658da9f1eb3abb8
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-12 01:02:20 +0800

    Time: 2 ms (93.38%) | Memory: 4.2 MB (32.75%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,11 +1,28 @@
 func twoSum(nums []int, target int) []int {
-    m := make(map[int]int, len(nums))
+    // init map
+    m := make(map[int]int)
 
     for i,v := range nums {
         if val, ok := m[target-v]; ok {
             return []int{i, val}
         }
+        // put in map
         m[v] = i
     }
+
     return nil
+
+
+
+
+
+    // m := make(map[int]int, len(nums))
+
+    // for i,v := range nums {
+    //     if val, ok := m[target-v]; ok {
+    //         return []int{i, val}
+    //     }
+    //     m[v] = i
+    // }
+    // return nil
 }

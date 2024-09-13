74493500360e57c26c7ca1ca5b2c1de8987c64ab
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-13 15:35:43 +0800

    Time: 8 ms (41.05%) | Memory: 3 MB (61.35%) - LeetSync

--- 242-valid-anagram/valid-anagram.go
+++ 242-valid-anagram/valid-anagram.go
@@ -1,21 +1,36 @@
-type sortRunes []rune
+// type sortRunes []rune
 
-func (s sortRunes) Less(i,j int) bool {
-    return s[i] < s[j]
-}
+// func (s sortRunes) Less(i,j int) bool {
+//     return s[i] < s[j]
+// }
 
-func (s sortRunes) Swap(i,j int) {
-    s[i], s[j] = s[j], s[i]
-}
+// func (s sortRunes) Swap(i,j int) {
+//     s[i], s[j] = s[j], s[i]
+// }
 
-func (s sortRunes) Len() int {
-    return len(s)
-}
+// func (s sortRunes) Len() int {
+//     return len(s)
+// }
 
 func isAnagram(s string, t string) bool {
-    sr, st := sortRunes(s), sortRunes(t)
-    sort.Sort(sr)
-    sort.Sort(st)
+    if len(s) != len(t) {return false}
 
-    return string(sr) == string(st)
+    m := make(map[rune]int)
+
+    for _, v := range s {
+        m[v] += 1
+    }
+
+    for _,v := range t {
+        if _, ok := m[v]; !ok {
+            return false
+        }
+        m[v] -= 1
+
+        if m[v] == 0 { delete(m,v) }
+    }
+
+    if len(m) != 0 {return false}
+
+    return true
 }

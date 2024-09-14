b9a6cd2d7b14b5440758553c6bb11a971101ebc7
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-14 15:30:10 +0800

    Time: 9 ms (65.38%) | Memory: 4.4 MB (7.69%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.go
@@ -0,0 +1,25 @@
+func removeCoveredIntervals(intervals [][]int) int {
+    // sort first element by increasing order
+    // sort second element by decreasing order
+    // this way we know that the first element of later elements
+    // are always covered by first element of previous elements
+    sort.Slice(intervals, func(i,j int) bool {
+        if intervals[i][0] == intervals[j][0] {
+            return intervals[i][1] > intervals[j][1]
+        }
+
+        return intervals[i][0] <= intervals[j][0]
+    })
+
+    i, count := 0, 0
+
+    for j := 1; j < len(intervals); j++ {
+        if intervals[i][1] < intervals[j][1] {
+            i = j
+            continue
+        }
+        count += 1
+    }
+
+    return len(intervals)-count
+}

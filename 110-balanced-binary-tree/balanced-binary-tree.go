cf3276a3b3df2c672d6f4e0cf4eed78548d564f0
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-14 22:39:15 +0800

    Time: 7 ms (45.46%) | Memory: 5.6 MB (9.18%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.go
+++ 110-balanced-binary-tree/balanced-binary-tree.go
@@ -7,36 +7,34 @@
  * }
  */
 func isBalanced(root *TreeNode) bool {
-    if root == nil {
-        return true
-    }
+    if root == nil {return true}
 
     return depth(root) != -1
+
 }
 
 func depth(root *TreeNode) int {
     if root == nil { return 0 }
-    left := depth(root.Left)
-    right := depth(root.Right)
 
-    if left == -1 || right == -1 || abs(left-right) > 1 {
+    left, right := depth(root.Left), depth(root.Right)
+
+    if (absInt(left-right) > 1 || left == -1 || right == -1) {
         return -1
     }
 
-    return max(left, right)+1
-
+    return maxInt(left, right) + 1
 }
 
-func max(a,b int) int {
-    if a > b {
-        return a
-    }
-    return b
-}
-
-func abs(a int) int {
+func absInt(a int) int {
     if a < 0 {
         return -a
+    }
+    return a
+}
+
+func maxInt(a, b int) int {
+    if a < b {
+        return b
     }
     return a
 }

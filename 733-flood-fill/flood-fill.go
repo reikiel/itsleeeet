9071aa2cd95fe326a62eb0698dcb4b0418b4b793
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-15 15:18:34 +0800

    Time: 7 ms (49.45%) | Memory: 3.9 MB (18.60%) - LeetSync

--- 733-flood-fill/flood-fill.go
+++ 733-flood-fill/flood-fill.go
@@ -1,28 +1,24 @@
 func floodFill(image [][]int, sr int, sc int, color int) [][]int {
     if image[sr][sc] == color { return image }
-    ogCol := image[sr][sc]
+    startCol := image[sr][sc]
     image[sr][sc] = color
 
     // up
-    if sr > 0 && image[sr-1][sc] == ogCol {
+    if sr > 0 && image[sr-1][sc] == startCol {
         floodFill(image, sr-1, sc, color)
     }
 
-    // down
-    if sr < len(image)-1 && image[sr+1][sc] == ogCol {
+    if sr < len(image)-1 && image[sr+1][sc] == startCol {
         floodFill(image, sr+1, sc, color)
     }
 
-    // left 
-    if sc > 0 && image[sr][sc-1] == ogCol {
+    if sc > 0 && image[sr][sc-1] == startCol {
         floodFill(image, sr, sc-1, color)
     }
 
-    // right
-    if sc < len(image[0])-1 && image[sr][sc+1] == ogCol {
+    if sc < len(image[0])-1 && image[sr][sc+1] == startCol {
         floodFill(image, sr, sc+1, color)
     }
-
 
     return image
 }

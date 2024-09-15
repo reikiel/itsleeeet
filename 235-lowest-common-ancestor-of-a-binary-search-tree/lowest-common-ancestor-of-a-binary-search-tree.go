f6e9a67d4955c2b041e09e33a32da35f558284a8
reikiel <43162731+reikiel@users.noreply.github.com>
2024-09-15 15:26:03 +0800

    Time: 19 ms (31.39%) | Memory: 7.4 MB (20.11%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -8,14 +8,19 @@
  */
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-    // if one is < root and other > root, or if either == root
-    // then thats the LCA
-    if root.Val == p.Val || root.Val == q.Val || (q.Val < root.Val && root.Val < p.Val) || (p.Val < root.Val && root.Val < q.Val) {
-        return root
-    } else if p.Val < root.Val && q.Val < root.Val {
-        return lowestCommonAncestor(root.Left, p, q)
-    } else { // both is more
-        return lowestCommonAncestor(root.Right, p, q)
+    if p.Val > q.Val {
+        q, p = p, q
     }
+
+    if p.Val <= root.Val && root.Val <= q.Val {
+        return root
+    }
+
+    if p.Val < root.Val && q.Val < root.Val {
+        return lowestCommonAncestor(root.Left, p, q)
+    }
+
+    return lowestCommonAncestor(root.Right, p, q)
+
 	
 }

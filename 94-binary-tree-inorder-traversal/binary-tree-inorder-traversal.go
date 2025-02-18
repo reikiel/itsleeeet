febe2bf46a3431310892d045e85e67f3398e2001
reikiel <43162731+reikiel@users.noreply.github.com>
2025-02-19 02:40:20 +0800

    Time: 0 ms (100.00%) | Memory: 4.2 MB (3.12%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.go
@@ -7,25 +7,12 @@
  * }
  */
 func inorderTraversal(root *TreeNode) []int {
-    stack := []*TreeNode{}
-    res := []int{}
-
-    for root != nil || len(stack) > 0 {
-        for root != nil {
-            stack = append(stack, root)
-            root = root.Left
-        }
-
-        // else
-        // pop last off stack and add to res, check if got right
-        // if no right then continue loop
-        // if have right then add that to the stack, and continue
-        root = stack[len(stack)-1]
-        stack = stack[:len(stack)-1]
-        res = append(res, root.Val)
-        
-        // dont need append here, will be appended in next iteration if not nil
-        root = root.Right
+    if root == nil {
+        return []int{}
     }
+    res := []int{}
+    res = append(res, inorderTraversal(root.Left)...)
+    res = append(res, root.Val)
+    res = append(res, inorderTraversal(root.Right)...)
     return res
 }

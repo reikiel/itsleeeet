3e0450945d8bd908d53690344b054e0ae03c56a7
reikiel <43162731+reikiel@users.noreply.github.com>
2025-02-19 15:13:16 +0800

    Time: 0 ms (100.00%) | Memory: 4.1 MB (18.72%) - LeetSync

---
+++ 27-remove-element/remove-element.go
@@ -0,0 +1,10 @@
+func removeElement(nums []int, val int) int {
+    i := 0
+    for _, x := range nums {
+        if x != val {
+            nums[i] = x
+            i++
+        }   
+    }
+    return i
+}
